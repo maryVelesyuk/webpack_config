34c3344c90878a088b212b6850b884181324d8fb
Maryia Velesiuk <m.velesiuk@andersenlab.com>
2023-06-26 10:39:13 +0300

    webpack configuration

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./style.scss";
+
+const App = () => {
+  return <h1 className={styles.content}>React and Webpack</h1>;
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,75 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+const mode = process.env.NODE_ENV || "development";
+const devMode = mode === "development";
+const target = devMode ? "web" : "browserslist";
+const devtool = devMode ? "source-map" : undefined;
+
+module.exports = {
+  mode,
+  target,
+  devtool,
+  entry: path.resolve(__dirname, "./src/index.js"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+    filename: "[name].[contenthash].js",
+  },
+  module: {
+    rules: [
+      { test: /\.html$/, loader: "html-loader" },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: { loader: "babel-loader" },
+      },
+      {
+        test: /\.(c|sc|sa)ss$/,
+        exclude: /node_modules/,
+        use: [
+          devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              sourceMap: devMode,
+            },
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [require("postcss-preset-env")],
+              },
+            },
+          },
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+    alias: {},
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Webpack and React",
+      template: path.resolve(__dirname, "src", "index.html"),
+      favicon: "./public/favicon.ico",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+    }),
+  ],
+  devServer: {
+    port: 3000,
+    open: true,
+    hot: true,
+  },
+};
